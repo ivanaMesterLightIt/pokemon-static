7d83150abf6de4f93c3954cab440003c0f4f576e
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-05 15:35:07 -0300

    progress class 54 section 4

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,21 +1,102 @@
-import { useRouter } from 'next/router';
 import * as React from 'react';
 import { Layout } from '../../components/layouts';
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
+import { pokeApi } from '../../api';
+import { Pokemon } from '../../interfaces';
+import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
 
 interface Props {
-    pokemon: any;
+    pokemon: Pokemon;
 }
 
-const PokemonPage = () => {
-    
-    const router = useRouter();
-    console.log(router.query);
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
     return (
         <Layout title='Algun pokemon'>
-            <h1>Hola mundo</h1>
+            <Grid.Container css={{ marginTop: '5px'}} gap={ 2 }>
+                <Grid xs={ 12 } sm={ 4 }>
+                    <Card hoverable css={{ padding: '30px'}}>
+                        <Card.Body>
+                            <Card.Image 
+                                src={pokemon.sprites.other?.dream_world.front_default || '/no-image.png'}
+                                alt={ pokemon.name }
+                                width="100%"
+                                height={ 200 }
+                            />
+                        </Card.Body>
+                    </Card>
+                </Grid>
+                <Grid xs={ 12 } sm={ 8 }>
+                    <Card>
+                        <Card.Header css={{ display: 'flex', justifyContent: 'space-between'}}>
+                            <Text h1 transform='capitalize'>{ pokemon.name }</Text>
+                            <Button color="gradient" ghost>
+                                Guardar en favoritos
+                            </Button>
+                        </Card.Header>
+                        <Card.Body>
+                            <Text size={30}>Sprites:</Text>
+                            <Container display='flex' direction='row' gap={0}>
+                                <Image 
+                                    src={ pokemon.sprites.front_default }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                                <Image 
+                                    src={ pokemon.sprites.back_default }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                                <Image 
+                                    src={ pokemon.sprites.front_shiny }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                                <Image 
+                                    src={ pokemon.sprites.back_shiny }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                            </Container>
+                        </Card.Body>
+                    </Card>
+                </Grid>
+            </Grid.Container>
         </Layout>
     );
+}
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+
+    const pokemons151 = [...Array(151)].map( ( value, index ) => `${ index + 1 }` );
+
+    return {
+      paths: pokemons151.map( id => ({
+        params: { id }
+      })),
+      fallback: false
+    }
+  }
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+
+    const { id } = params as { id: string };
+
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`, {
+      headers: {
+        'accept-encoding': '*'
+      }
+    });
+  
+    return {
+      props: {
+        pokemon: data
+      }
+    }
 }
 
 export default PokemonPage;
