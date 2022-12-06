d688fe750be79ad6362945984d43b1811eb285d5
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-06 12:25:14 -0300

    progress class 66 section 5

--- pages/favourites/index.tsx
+++ pages/favourites/index.tsx
@@ -1,3 +1,4 @@
+import { Card, Grid } from '@nextui-org/react';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Layout } from '../../components/layouts';
@@ -14,7 +15,27 @@ const FavouritesPage = () => {
 
     return (
         <Layout title='Pokemons - Favoritos'>
-            <NoFavourites />
+            {
+                favouritePokemos.length === 0 
+                    ? (<NoFavourites />)
+                    : (
+                        <Grid.Container gap={ 2 } direction='row' justify='flex-start'>
+                            {
+                                favouritePokemos.map( id => (
+                                    <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ id }>
+                                        <Card hoverable clickable css={{ padding: 10 }}>
+                                            <Card.Image 
+                                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                                                width={'100%'}
+                                                height={ 140 }
+                                            />
+                                        </Card>
+                                    </Grid>
+                                ))
+                            }
+                        </Grid.Container>
+                    )
+            }
         </Layout>
     );
 }
